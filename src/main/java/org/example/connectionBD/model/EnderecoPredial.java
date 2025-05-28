6022cffe2ebab6ce3754e055c78a3c716ff519a1
Lucas da Costa <49735129+lucascostadevrobot@users.noreply.github.com>
2025-05-28 14:41:04 -0300

    IV02 Tarefa - Mapeando as Entidades v1

---
+++ src/main/java/org/example/connectionBD/model/EnderecoPredial.java
@@ -0,0 +1,108 @@
+package org.example.connectionBD.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class EnderecoPredial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idEndereco;
+
+    @Column
+    private String rua;
+    @Column
+    private Integer numero;
+    @Column
+    private String bairro;
+    @Column
+    private String cidade;
+    @Column
+    private String complemento;
+    @Column
+    private String numeroApartamento;
+    @OneToMany
+    private List<Proprietarios> proprietariosList;
+
+    public EnderecoPredial(Integer idEndereco, String rua, Integer numero, String bairro, String complemento, String cidade, String numeroApartamento) {
+        this.idEndereco = idEndereco;
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.complemento = complemento;
+        this.cidade = cidade;
+        this.numeroApartamento = numeroApartamento;
+    }
+
+    public EnderecoPredial() {
+    }
+
+    public Integer getIdEndereco() {
+        return idEndereco;
+    }
+
+    public void setIdEndereco(Integer idEndereco) {
+        this.idEndereco = idEndereco;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getNumeroApartamento() {
+        return numeroApartamento;
+    }
+
+    public void setNumeroApartamento(String numeroApartamento) {
+        this.numeroApartamento = numeroApartamento;
+    }
+
+    @Override
+    public String toString() {
+        return "EnderecoPredial{" +
+                "idEndereco=" + idEndereco +
+                ", rua='" + rua + '\'' +
+                ", numero=" + numero +
+                ", bairro='" + bairro + '\'' +
+                ", cidade='" + cidade + '\'' +
+                ", complemento='" + complemento + '\'' +
+                ", numeroApartamento='" + numeroApartamento + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/connectionBD/model/Proprietarios.java
@@ -0,0 +1,91 @@
+package org.example.connectionBD.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Proprietarios {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String nome;
+    @Column
+    private Integer idade;
+    @Column
+    private boolean ativo;
+
+    @Column
+    @ManyToOne
+    private EnderecoPredial enderecoPredial;
+
+    public Proprietarios(Integer id, String nome, Integer idade, boolean ativo) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.ativo = ativo;
+    }
+
+    public Proprietarios(String nome, Integer idade, boolean ativo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.ativo = ativo;
+    }
+
+
+    public Proprietarios() {
+    }
+
+
+    public EnderecoPredial getEnderecoPredial() {
+        return enderecoPredial;
+    }
+
+    public void setEnderecoPredial(EnderecoPredial enderecoPredial) {
+        this.enderecoPredial = enderecoPredial;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    @Override
+    public String toString() {
+        return "Proprietarios{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", ativo=" + ativo +
+                ", enderecoPredial=" + enderecoPredial +
+                '}';
+    }
+}
